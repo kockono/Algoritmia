488fc8633817bc34df7ecd18f4210f2d89c8de8b
kockono <kockonoop@gmail.com>
2023-04-09 00:56:16 -0600

    Achievement: longest-common-letters

---
+++ Easy Algorithms/longest-common-letters.ts
@@ -0,0 +1,34 @@
+function longestCommonPrefix(strs: string[]): string {
+  const primerPalabra = strs[0];
+  let letrasEnComun:any[] = []
+
+  if( strs.length === 1 ) return primerPalabra.charAt(0);
+
+   for (let i = 0; i < strs.length -1; i++) {
+      compararPalabra(primerPalabra, strs[i+1], letrasEnComun);
+   }
+
+   if( letrasEnComun.length === 1 ) return strs[0];
+   return encontrarLaPalabraMasLargaEnComun(letrasEnComun);;
+};
+
+longestCommonPrefix(["flower","flow","flight"]);
+
+function compararPalabra(primerPalabra:string, siguientePalabra:string, letrasEnComun:any[]) { 
+  let letrasEncontradas:any[] = []
+  let sizeComparasion = primerPalabra.length > siguientePalabra.length ? siguientePalabra.length : primerPalabra.length;
+
+  for (let j = 0; j < sizeComparasion; j++) {
+    primerPalabra.charAt(j) === siguientePalabra.charAt(j) ? letrasEncontradas.push(primerPalabra.charAt(j)) : null;
+  }
+  letrasEnComun.push(letrasEncontradas);
+  console.log("Letras Encontradas: "+ letrasEncontradas )
+}
+
+function encontrarLaPalabraMasLargaEnComun(letrasEnComun:any[]) {
+  // Si la palabra a es menor que la palabra b, entonces a es la palabra mas corta
+  let prefijoComun = letrasEnComun.reduce((a, b) => a.length > b.length ? b : a).join("");
+  console.log( "prefijoComun" );
+  console.log( prefijoComun );
+  return prefijoComun;
+} 
