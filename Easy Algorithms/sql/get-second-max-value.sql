57d049d0c71dc1b342e59110221b1b69b614abd8
kockono <kockonoop@gmail.com>
2023-04-11 22:22:59 -0600

    feat: get second max value

---
+++ Easy Algorithms/sql/get-second-max-value.sql
@@ -0,0 +1,7 @@
+-- Description: Write a SQL query to get the second highest salary from the Employee table.
+-- Author: Chris Cobos
+-- Obtiene primero el salario maximo después compara con el salario maximo  y si este es menor al maximo entonces obtiene segundo salario maximo
+
+select max(salary) as SecondHighestSalary 
+      from  Employee 
+      where salary < (SELECT MAX(salary) FROM Employee);
