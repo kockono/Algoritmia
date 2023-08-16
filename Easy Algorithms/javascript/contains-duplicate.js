0780c62698b0f52780f9f3cf5670f94b92ee86f6
kockono <kockonoop@gmail.com>
2023-08-16 02:09:42 -0600

    feat(ContainsDuplicates): :sparkles: resolved

---
+++ Easy Algorithms/javascript/contains-duplicate.js
@@ -0,0 +1,18 @@
+/**
+ * Problema 217
+ * @description Dado un array de enteros, determinar si el array contiene duplicados.
+ * @param {number[]} nums array de enteros
+ * @returns {boolean} si el array contiene duplicados retornara un tru y false en caso contrario.
+ * Solucion: Se recorre el array y se compara cada elemento con los demas elementos del array, si se encuentra un elemento igual se retorna true.
+ */
+var containsDuplicates = (nums) => {
+    for (let i = 0; i < nums.length; i++) {          
+        for (let j = 0; j < nums.length; j++) {
+           // Compara la posición i con la posición j, si son iguales y no son la misma posición retorna true.
+            if (i !== j && nums[i] === nums[j]) return true;
+        }
+    }
+    
+    return false;
+
+};
