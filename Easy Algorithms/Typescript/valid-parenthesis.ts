91a2b5c99269c30c5cdf1cf1def85d5fa4022466
kockono <kockonoop@gmail.com>
2023-08-21 01:38:58 -0600

    feat(Documentation): :sparkles: update

---
+++ Easy Algorithms/Typescript/valid-parenthesis.ts
@@ -0,0 +1,12 @@
+/**
+ * Lo que tenemos que hacer es que ciere al mismo tiempo
+ * https://leetcode.com/problems/valid-parentheses/
+ * 20#
+ */
+function isValid(s: string): boolean {
+
+    let parentesis:string[] = s.split("");
+    
+    return true;
+    
+};
