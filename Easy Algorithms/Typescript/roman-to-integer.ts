b85651bad5fc1c5635f7ea0423e4856d330050a2
kockono <kockonoop@gmail.com>
2023-09-06 23:50:05 -0600

    feat(RomanIntegers): :sparkles:

--- Easy Algorithms/Typescript/roman-to-integer.ts
+++ Easy Algorithms/Typescript/roman-to-integer.ts
@@ -1,6 +1,22 @@
-
+/**
+ * 
+ * 
+ * @param s 
+ * @returns 
+ * 
+ * I can be placed before V (5) and X (10) to make 4 and 9. 
+ * X can be placed before L (50) and C (100) to make 40 and 90. 
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ */
 function romanToInt(s: string): number {
-  const Dictionary:any = { 
+
+  let numberRomans = s.split('');
+  
+  let resultado = 0;
+  let anterior  = 0;
+  let actual    = 0;
+
+  const romanNumbers:any = { 
     I: 1,
     V: 5,
     X: 10,
@@ -8,48 +24,38 @@ function romanToInt(s: string): number {
     C: 100,
     D: 500,
     M: 1000,
-    CM: 900,
-    XC: 90,
-    IV: 4
   }
-  let numeros:string | number[] = [];
-  let letrasRomanas = s.toString();
 
-for (let i = 0; i < letrasRomanas.length; i++) {
-    if( letrasRomanas.charAt(i) === "I" && letrasRomanas.charAt(i+1) === "V" ) {
-      console.log("4");
-      numeros.push(4);
-      i++;
+  for (let i = s.length; i > 0; i--) {
+
+    anterior = romanNumbers[s[i-2]] || 0;
+    actual   = romanNumbers[s[i-1]];
+
+    if(actual === anterior){
+      resultado += actual;
+    }else if(actual  === 5 && anterior === 1){
+      resultado += 4;
+      i--;
+    }else if(actual  === 10 && anterior === 1){
+      resultado += 9;
+      i--;
+    }else if(actual  === 50 && anterior === 10){
+      resultado += 40;
+      i--;
+    }else if(actual  === 100 && anterior === 10){
+      resultado += 90;
+      i--; 
+    }else if(actual  === 500 && anterior === 100){
+      resultado += 400;
+      i--;
+    }else if(actual  === 1000 && anterior === 100){
+      resultado += 900;
+      i--;
+    }else{
+      resultado += actual;
     }
-    if (letrasRomanas.charAt(i) === "I" && letrasRomanas.charAt(i+1) === "X") {
-      console.log( "9" )
-      numeros.push(9);
-      i++;
-    }
-    if (letrasRomanas.charAt(i) === "X" && letrasRomanas.charAt(i+1) === "L") {
-      console.log( "40" )
-      numeros.push(40);
-      i++;
-    }
-    if (letrasRomanas.charAt(i) === "X" && letrasRomanas.charAt(i+1) === "C") {
-      console.log( "90" )
-      numeros.push(90);
-      i++;
-    }
-    if (letrasRomanas.charAt(i) === "C" && letrasRomanas.charAt(i+1) === "D") {
-      console.log( "400" )
-      numeros.push(400);
-      i++;
-    }
-    if (letrasRomanas.charAt(i) === "C" && letrasRomanas.charAt(i+1) === "M") { 
-      console.log( "900" )
-      numeros.push(900);
-      i++;
-    }else {
-      numeros.push(Dictionary[letrasRomanas.charAt(i)]);
-    }
-     numeros.map((item:any) => item += item);
-     console.log( numeros )
-}
-  return 5;
+
+  }
+
+  return resultado;
 };
